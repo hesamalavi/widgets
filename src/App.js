5cba2668a563c3b125b43d74d17810e55925e5f5
Hesam Alavi <hesamalavi@Hesams-MacBook-Pro.local>
2020-11-04 18:57:25 +1100

    #

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import Accordion from './components/Accordion';
+// import Accordion from './components/Accordion';
+// import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
-const items = [
+// const items = [
+//     {
+//         title: 'What is React',
+//         content: 'React is a JS framework',
+//     },
+//     {
+//         title: 'how do we use React',
+//         content: 'Love it',
+//     },
+//     {
+//         title: 'How do you use React',
+//         content: 'By creating components',
+//     },
+// ];
+
+const options = [
     {
-        title: 'What is React',
-        content: 'React is a JS framework',
+        label: 'The Color Red',
+        value: 'red',
     },
     {
-        title: 'how do we use React',
-        content: 'Love it',
+        label: 'The color Green',
+        value: 'green',
     },
     {
-        title: 'How do you use React',
-        content: 'By creating components',
+        label: 'A Shade of Blue',
+        value: 'blue',
     },
 ];
 
 const App = () => {
     return (
         <div>
-            <Accordion items={items} />
+            <Dropdown options={options} />
         </div>
     );
 };
